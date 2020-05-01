09a0eac49d0408ca5505e4ca3b8fd2bb73c33fca
lizet milagros <lcuevas@unsa.edu.pe>
2020-04-30 22:36:08 -0500

    cambio

--- fib.java
+++ fib.java
@@ -3,10 +3,7 @@ class Fib{
     	public static void main(String []args){
 		Scanner sc=new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println( liz(n));
+		System.out.println(n);
 	}
-	public static int liz(int n){
-		if( n == 0 || n == 1 ) return 1;
-		return liz( n - 1 ) + liz( n - 2 );
-	}
+	
 }

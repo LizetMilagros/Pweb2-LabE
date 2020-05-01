e57ca37c626ee5e7acbeee1c3be89ee4ba706df3
lizet milagros <lcuevas@unsa.edu.pe>
2020-04-30 22:04:49 -0500

    corregi errores sintacticos

--- fib.java
+++ fib.java
@@ -1,11 +1,12 @@
+import java.util.*;
 class Fib{
-    	public static void main(string []ags){
+    	public static void main(String []args){
 		Scanner sc=new Scanner(System.in);
 
 		int n = sc.nextInt();
 		System.out.println( fib(n));
 	}
-	public int fib(int n){
+	public static int fib(int n){
 		if( n == 0 || n == 1 ) return 1;
 		return fib( n - 1 ) + fib( n - 2 );
 	}
